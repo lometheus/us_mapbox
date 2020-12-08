fa4a2d9e82c584c8dde1fa419c4b1f63505b423a
louzhengyu1 <louzhengyu1@360.cn>
2020-12-08 17:57:51 +0800

    init

--- GeoJSON factory.py
+++ GeoJSON factory.py
@@ -20,9 +20,7 @@ df2=df2.dropna()
 df2['FIPS'] = df2['fips'].apply(lambda x: str(int(x)).zfill(5))
 df2['mounth']=df2['date'].apply(lambda x:int(str(x).split("/")[1]))
 
-df2=df2.groupby([df2['mounth'],df2['county']],as_index=False).max()
-
-
+df2=df2.groupby([df2['mounth'],df2['FIPS']],as_index=False).max()
 
 BINS = [
     "0",
@@ -46,34 +44,34 @@ BINS = [
 def map_bins(x):
     if x==0:
         return "0"
-    elif x in range(1,500):
+    elif x in range(1,501):
         return "1-500"
-    elif x in range(501,1000):
+    elif x in range(501,1001):
         return "501-1000"
-    elif x in range(1001,1500):
+    elif x in range(1001,1501):
         return "1001-1500"
-    elif x in range(1501,2200):
+    elif x in range(1501,2201):
         return "1501-2200"
-    elif x in range(2201,3000):
+    elif x in range(2201,3001):
         return "2201-3000"
-    elif x in range(3001,4000):
+    elif x in range(3001,4001):
         return "3001-4000"
-    elif x in range(4001,5000):
+    elif x in range(4001,5001):
         return "4001-5000"
-    elif x in range(5001,6000):
+    elif x in range(5001,6001):
         return "5001-6000"
-    elif x in range(6001,7000):
+    elif x in range(6001,7001):
         return "6001-7000"
-    elif x in range(7001,8000):
+    elif x in range(7001,8001):
         return "7001-8000"
-    elif x in range(8001,9000):
+    elif x in range(8001,9001):
         return "8001-9000"
-    elif x in range(9001,10000):
+    elif x in range(9001,10001):
         return "9001-10000"
-    elif x in range(10001,11500):
+    elif x in range(10001,11501):
         return "10001-11500"
-    elif x in range(11500,13000):
-        return "11500-13000"
+    elif x in range(11501,13000):
+        return "11501-13000"
     elif x >=13001:
         return "-13001"
 
@@ -90,9 +88,11 @@ for mon in Mounths:
     df_single_mounth = df2[(df2.mounth <= mon)]
 
     merged = dfg.merge(df_single_mounth, right_on='FIPS', left_on='id' )
+    record=[]
     for bin in bins:
         print(mon,bin)
         geo_layer = merged[(merged['bins'] == bin)].drop(['bins','mounth','FIPS'],axis = 1)
+        # geo_layer=pd.merge(geo_layer,df3,how = 'right',on='FIPS').fillna(0)
         # try:
         if geo_layer.empty:
             continue
